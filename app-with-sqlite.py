c4d111f9fff15d14c31dfac973d92e7946387fbd
Yasin <157028958+yasindevops@users.noreply.github.com>
2024-02-13 11:09:03 +0300

    Update app-with-sqlite.py

--- app-with-sqlite.py
+++ app-with-sqlite.py
@@ -79,7 +79,7 @@ def add_email():
             return render_template('add-email.html', show_result=False)
 
 
-# - Add a statement to run the Flask application which can be reached from any host on port 80.
+# - Add a statement to run the Flask application which can be reached from any host on port 80:
 if __name__=='__main__':
     app.run(debug=True)
     app.run(host='0.0.0.0', port=8080)
